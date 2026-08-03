30afd662c5852f209e4c2fe7aec0288db6ce0ea1
Dionnell Riquelme <driquelme@globalterrenos.cl>
2026-08-03 15:18:38 -0400

    feat: implement authentication routes and loading states for user access control

--- src/leadsglobal/app/app.router.tsx
+++ src/leadsglobal/app/app.router.tsx
@@ -1,13 +1,18 @@
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router"
 import { DashboardLayout } from "../admin/layout/DashboardLayout"
 import { DashboardPage, Leads } from "../admin/pages"
+import { LoginPage } from "../auth/pages/LoginPage"
+import { AuthLayout } from "../auth/layout/AuthLayout"
+import { AdminRoute, NotAuthenticatedRoute } from "@/components/routes/AuthenticatedRoute"
 
 
 const appRouter = createBrowserRouter([
     //Dasboard routes
     {
         path: '/dashboard',
-        element: <DashboardLayout/>,
+        element: <AdminRoute>
+                    <DashboardLayout/>
+                 </AdminRoute>,
         children: [
             {
                 index: true,
@@ -16,6 +21,24 @@ const appRouter = createBrowserRouter([
             {
                 path: 'leads',
                 element: <Leads/>
+            }
+        ]
+    },
+
+    //Auth Routes
+    {
+        path: '/auth',
+        element: <NotAuthenticatedRoute>
+                    <AuthLayout/>
+                 </NotAuthenticatedRoute>,
+        children: [
+            {
+                index: true,
+                element: <Navigate to='/auth/login' />
+            },
+            {
+                path: 'login',
+                element: <LoginPage/>
             }
         ]
     },
